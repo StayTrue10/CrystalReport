7826239ec18e487a881eeeb52bd4e3329a1e5313
Kabir Alberto <35808948+StayTrue10@users.noreply.github.com>
2018-03-13 15:32:05 -0500

    v2
    + librerias innecesarias

--- ConsultaSQL.cs
+++ ConsultaSQL.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace reportecrystal
 {
     class ConsultaSQL
     {
         //String de conexion a sql server, dataset y datatable para datos de sql
-        private SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = ANCONA_PROD; user id=sa; password=Pr0c3s0.12");
-        private DataSet ds;
+        SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = ANCONA_PROD; user id=sa; password=Pr0c3s0.12");
+        DataSet ds;
         //Datos en dgv1, abre conexion, selecciona columnas de la bd, rellena el dataset, cierra conexion y regresa la tabla con los datos
         public DataTable MostrarDatos()
         {
