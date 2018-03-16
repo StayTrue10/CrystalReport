0811c68d7c6470d90476f4db14b8979a0db3c267
Kabir Alberto <35808948+StayTrue10@users.noreply.github.com>
2018-03-16 17:26:19 -0500

    v3
    + Busca registros y los muesta en el dgv, permite seleccionar filas y crea los reportes (1 por registro)
    + Archivo ConsultaSQL no es necesario.

--- Form1.cs
+++ Form1.cs
@@ -1,55 +1,67 @@
 using System;
 using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace reportecrystal
 {
     public partial class Form1 : Form
     {
-        //Clase ConsultaSQL, dataset y datatable nuevos para los datos
-        ConsultaSQL sql = new ConsultaSQL();
-        DataSet ds = new DataSet();
-        DataTable dt = new DataTable();
         public Form1()
         {
             InitializeComponent();
-            //Datos del servidor & nueva tabla, (tablas del mismo tamaño)
-            dataGridView1.DataSource = sql.MostrarDatos();
+        }
+        void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //Conexión, busca registros iguales, los agrega a una tabla y se asigna la vista al dgv
+            SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = ANCONA_PROD; user id=sa; password=Pr0c3s0.12");
+            SqlDataAdapter datos = new SqlDataAdapter("Select DisTinct T2.ItemCode, T2.Dscription,t2.Quantity, t3.Codebars, T1.Docnum, T1.DocDate, T1.DocTime, t2.DocEntry,T2.LineNum," +
+            "t1.numatcard FROM ODRF T1 INNER JOIN DRF1 T2 ON T1.DocEntry = T2.DocEntry INNER JOIN OITM T3 ON T3.ItemCode = T2.ItemCode where T2.ItemCode LIKE '%" + this.tbBuscar.Text + "%'", conexion);
+            DataSet data = new DataSet();
+            datos.Fill(data, "tabla");
+            dataGridView1.DataSource = data.Tables["tabla"];
+        }
+        void btnImprimir_Click(object sender, EventArgs e)
+        {
+            //Tabla para guardar las filas seleccionadas (Value = 1), agrega n filas de la la misma fila (Quantity)
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
             dt.Columns.Add("ItemCode", typeof(string));
             dt.Columns.Add("Dscription", typeof(string));
-            dt.Columns.Add("Quantity", typeof(string));
+            dt.Columns.Add("Quantity", typeof(Int32));
+            dt.Columns.Add("Codebars", typeof(string));
+            dt.Columns.Add("Docnum", typeof(Int32));
+            dt.Columns.Add("DocDate", typeof(string));
+            dt.Columns.Add("DocTime", typeof(string));
+            dt.Columns.Add("DocEntry", typeof(Int32));
+            dt.Columns.Add("LineNum", typeof(Int32));
+            dt.Columns.Add("NumatCard", typeof(string));
+            foreach (DataGridViewRow dgv in dataGridView1.Rows)
+            {
+                if (Convert.ToInt32(dgv.Cells[0].Value) == 1)
+                {
+                    for (int i = 0; i < Convert.ToInt32(dgv.Cells[3].Value); i++)
+                        dt.Rows.Add(dgv.Cells[1].Value, dgv.Cells[2].Value, dgv.Cells[3].Value, dgv.Cells[4].Value, dgv.Cells[5].Value, dgv.Cells[6].Value, dgv.Cells[7].Value, dgv.Cells[8].Value, dgv.Cells[9].Value, dgv.Cells[10].Value);
+                }
+            }
+            //Agrega la tabla con los datos seleccionados, crea archivo xml para lectura de los datos en Crystal, nuevo reporte, asignacion del DataSet y el Viewer
+            ds.Tables.Add(dt);
+            ds.WriteXmlSchema("Sample.xml");
+            CrystalReport1 cr = new CrystalReport1();
+            cr.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = cr;
         }
         void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             //Click solo en la primera columna (CheckBox)
             if (e.ColumnIndex == 0 && e.RowIndex >= 0)
             {
-                //Verifica valor, 1=Seleccionado (int o bool) y cambia el valor de la primera columna
+                //Verifica si esta seleccionada una fila y cambia de valor al seleccionar una fila
                 if (Convert.ToInt32(dataGridView1[0, e.RowIndex].Value) == 0)
                     dataGridView1[0, e.RowIndex].Value = 1;
                 else
                     dataGridView1[0, e.RowIndex].Value = 0;
             }
-        }
-        void btnImprimir_Click(object sender, EventArgs e)
-        {
-            //Limpia los datos de la tabla, recorre todas la filas y agrega solo las filas seleccionadas omitiendo la primera columna, agrega n cantidad de la misma fila
-            dt.Clear();
-            foreach (DataGridViewRow dgv in dataGridView1.Rows)
-            {
-                if (Convert.ToInt32(dgv.Cells[0].Value) == 1)
-                {
-                    for (int i = 0; i < Convert.ToInt32(dgv.Cells[3].Value); i++)
-                        dt.Rows.Add(dgv.Cells[1].Value, dgv.Cells[2].Value, dgv.Cells[3].Value);
-                }
-            }
-            //Reconstruye la tabla, agrega la tabla con los datos seleccionados, crea archivo xml para lectura de los datos en crystal, nuevo reporte, asignacion del dataset y el viewer 
-            ds.Tables.Clear();
-            ds.Tables.Add(dt);
-            ds.WriteXmlSchema("Sample.xml");
-            CrystalReport1 cr = new CrystalReport1();
-            cr.SetDataSource(ds);
-            crystalReportViewer1.ReportSource = cr;
         }
     }
 }
